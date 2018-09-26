c7edde68a73541202f22c1a2f42093c27be2dee5
Kenny Jarnagin <jarnagink19@hanover.edu>
2018-09-25 23:32:21 -0400

    Finish objective 1, assignment 2

--- assignments/assignment2.py
+++ assignments/assignment2.py
@@ -12,3 +12,10 @@ printContent(r)         # Prints the json body of the request
 r.headers               # A dictionary of the provided headers
 
 ## Write your code in what follows. You can include comments describing what you are doing.
+
+#Objective 1
+user_name = requests.get("http://127.0.0.1:5000/user")
+random_user = user_name.text[27:32]
+address = "http://127.0.0.1:5000/"
+user_name = requests.get(address + "user/" + random_user)
+print(user_name.text)
